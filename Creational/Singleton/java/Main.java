43127f26f999293452239c10a07c1ed347186990
Adriano Perlini <aperlini@protonmail.ch>
2024-01-16 20:14:04 +0100

    Singleton in Java

---
+++ Creational/Singleton/java/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+	public static void log(Object o) {
+		System.out.println(o);
+	}
+
+	public static void main(String[] args) {
+		Singleton s = Singleton.getInstance("new instance");
+		log(s);
+	}
+
+}
---
+++ Creational/Singleton/java/Singleton.java
@@ -0,0 +1,22 @@
+class Singleton {
+
+	private static Singleton instance;
+	private String data;
+	
+	private Singleton(String data) {
+		this.data = data;
+	}
+
+	public static Singleton getInstance(String data) {
+		if(instance == null) {
+			instance = new Singleton(data);
+		} 
+		return instance;
+	}
+
+	@Override
+	public String toString() {
+		return "Singleton(data="+this.data+")";
+	}
+};
+
